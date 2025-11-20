169755ab636947811e601ecf5a61803cfa36a2ed
Oyunbat <oyunbat9958@gmail.com>
2025-11-21 00:38:24 +0800

    testimonial , header

---
+++ app/components/Header.tsx
@@ -0,0 +1,65 @@
+"use client"
+import { useState, useEffect } from "react"
+import { Roboto, Ubuntu } from "next/font/google";
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: [
+        "100", "300", "400", "500", "700", "900" // add the weights you need
+    ],
+    style: ["normal", "italic"]
+});
+export const ubuntu = Ubuntu({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "700"],
+    style: ["normal", "italic"]
+});
+function Header() {
+    const [isMobile, setIsMobile] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false)
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50)
+        }
+        handleScroll();
+        handleResize();
+        window.addEventListener("scroll", handleScroll, { passive: true })
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+            window.removeEventListener("resize", handleResize)
+
+        }
+    }, []);
+
+    return (
+        <div className="w-screen h-[60px] fixed">
+            <div className="flex w-full justify-around p-4">
+                {isMobile ? <div className="fixed w-full flex h-[60px] justify-around items-center">
+                    <h1 className={`${ubuntu.className} font-medium text-xl`}>Ai.marketing</h1>
+                    <div className={`${ubuntu.className} font-medium text-xl`}>Menu</div>
+                </div> : <div className=" flex flex-col fixed h-20  w-full justify-around items-center top-4">
+                    <div className="flex w-full justify-around gap-180 ">
+                        <h1 className={`${ubuntu.className} font-medium text-xl`}>Ai.marketing</h1>
+                        <div>
+                            <h1 className={`${ubuntu.className} font-medium text-xl`}>Холбоо барих</h1>
+                        </div>
+                    </div>
+                    <div className={`${isScrolled ? "opacity-0 transition-all duration-200 " : "flex items-center justify-center transition-all duration-200 "}`}>
+                        <nav>
+                            <ul className={`${ubuntu.className} font-medium text-xl flex items-center justify-center gap-20`} >
+                                <li>Үндсэн цэс </li>
+                                <li>Бидний тухай </li>
+                                <li>Үйлчилгээ</li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>}
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,60 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { useState, useEffect } from "react"
+import { Ubuntu } from "next/font/google";
+
+export const ubuntu = Ubuntu({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "700"],
+    style: ["normal", "italic"]
+});
+export default function HeroSection() {
+    const [isTablet, setIsTablet] = useState(false);
+    const arr = [1, 2, 3, 4]
+    useEffect(() => {
+        const handleResize = () => {
+            setIsTablet(window.innerWidth >= 728);
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize();
+    }, []);
+    return (
+        <div className="h-screen w-full flex flex-col justify-center items-center gap-8 m-[0px_20px]">
+            {isTablet ? <div className="flex  justify-center w-[1200px] h-screen items-center gap-4 pl-[100px]">
+                <div className="flex flex-col w-full justify-center gap-4  ">
+                    <h1 className={`${ubuntu.className} font-medium text-[32px] md:text-[42px] lg:text-[62px] xl:text-[82px]`}>Хиймэл оюун & Digital marketing</h1>
+                    <p className={`${ubuntu.className} md:text-[20px] w-[400px]`}>Та өөрийн бизнесээ бидэнтэй хамтарч бүрэн автоматжуулаарай.</p>
+                    <Button className={`${ubuntu.className} border w-[180px] h-[60px] border-black text-black`}>Бидний тухай</Button>
+                </div>
+                <div className="flex flex-col gap-10">
+                    <div className="flex gap-1">  {arr.map((item) => (
+                        <div key={item} className="w-22 h-22 sm:w-28 sm:h-28   bg-[#ccff34]"></div>
+                    ))}
+                    </div>
+                    <div className="flex gap-1">  {arr.map((item) => (
+                        <div key={item} className="w-22 h-22  sm:w-28 sm:h-28   bg-[#ccff34]"></div>
+                    ))}
+                    </div>
+                    <div className="flex gap-1">  {arr.map((item) => (
+                        <div key={item} className="w-22 h-22  sm:w-28 sm:h-28   bg-[#ccff34]"></div>
+                    ))}
+                    </div>
+                </div>
+            </div> : <div className="flex flex-col justify-center items-center gap-4">            <div className="flex flex-col justify-center items-center gap-4">
+                <h1 className={`text-center text-[32px] ${ubuntu.className}`}>Та өөрийн бизнесээ бидэнтэй хамтарч бүрэн автоматжуулаарай. </h1>
+                <Button className={`border w-[180px] h-[60px] border-black text-black ${ubuntu.className}`}>Бидний тухай</Button>
+            </div>
+                <div className="flex flex-col gap-2">
+                    <div className="flex gap-1">  {arr.map((item) => (
+                        <div key={item} className="w-22 h-22 sm:w-28 sm:h-28    bg-[#ccff34]"></div>
+                    ))}
+                    </div>
+                    <div className="flex gap-1">  {arr.map((item) => (
+                        <div key={item} className="w-22 h-22  sm:w-28 sm:h-28   bg-[#ccff34]"></div>
+                    ))}
+                    </div>
+                </div>
+            </div>}
+        </div >
+    )
+}
